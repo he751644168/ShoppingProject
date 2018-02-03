c6d93d2806867d99d2affbb270bba089c0a60a40
hlk <18371861831@163.com>
2018-02-03 22:02:16 +0800

    主要对公共代码的修改，此处api短时间不需要做二次修改

--- src/main/java/com/shopping/servlet/color.java
+++ src/main/java/com/shopping/servlet/color.java
@@ -1,7 +1,11 @@
 package com.shopping.servlet;
 
+import java.time.LocalDateTime;
+
 public class color {
     public static void main(String[] args) {
+        LocalDateTime localDateTime=LocalDateTime.now();
+        System.out.println(localDateTime.getYear());
 
     }
 }
