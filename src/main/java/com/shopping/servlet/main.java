0cf728585c70d3efa4e93b6a31652c58d2bfae70
hlk <18371861831@163.com>
2018-02-03 20:34:36 +0800

    今天又添加一行

--- src/main/java/com/shopping/servlet/main.java
+++ src/main/java/com/shopping/servlet/main.java
@@ -4,20 +4,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        System.out.println("跑起来");
         req.getRequestDispatcher("/main.jsp").forward(req, resp);
+        List<String> list = new ArrayList<String>();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         this.doGet(req, resp);
-        System.out.println("123");
     }
 }
---
+++ src/main/java/com/shopping/servlet/t.java
@@ -0,0 +1,10 @@
+package com.shopping.servlet;
+
+public class t {
+    public static void main(String[] args) {
+        int a=10;
+        int k=12;
+        int j=a+++k-5;
+
+    }
+}
