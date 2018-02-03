ddb0f5c598b49d03b87fd0259fbd20d9d29de671
hlk <18371861831@163.com>
2018-02-03 20:44:06 +0800

    111111

---
+++ src/main/java/com/shopping/servlet/color.java
@@ -0,0 +1,7 @@
+package com.shopping.servlet;
+
+public class color {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/shopping/servlet/t.java
+++ src/main/java/com/shopping/servlet/t.java
@@ -1,10 +1,17 @@
 package com.shopping.servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class t {
     public static void main(String[] args) {
-        int a=10;
-        int k=12;
-        int j=a+++k-5;
-
+        int a = 10;
+        int k = 12;
+        int j = a++ + k - 5;
+        System.out.println(j);
+        List<String> list = new ArrayList<String>();
+        for (String c:list) {
+            System.out.println(c);
+        }
     }
 }
